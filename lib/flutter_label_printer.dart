a01a43981a9bcdaa9f962041b825748087f17599
Peter WONG <peter.wong@gogotech.hk>
2023-01-13 15:24:00 +0800

    remove unneeded code from template

--- lib/flutter_label_printer.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_label_printer/flutter_label_printer_platform_interface.dart';
-
-class FlutterLabelPrinter {
-  Future<String?> getPlatformVersion() => FlutterLabelPrinterPlatform.instance.getPlatformVersion();
-}
