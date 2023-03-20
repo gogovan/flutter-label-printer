de1cdab32b526184695c4ce83e5b006035203820
Peter WONG <peter.wong@gogotech.hk>
2023-03-20 17:26:09 +0800

    update format and readme

--- lib/templating/string_replacer.dart
+++ lib/templating/string_replacer.dart
@@ -2,7 +2,7 @@ extension StringReplacer on String {
   String format(Map<String, String> replace) {
     var result = this;
     for (final entry in replace.entries) {
-      result = result.replaceAll('{${entry.key}}', entry.value);
+      result = result.replaceAll('{{${entry.key}}}', entry.value);
     }
 
     return result;
